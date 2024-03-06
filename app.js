a3ea2487b220ca21642b6f6e2a3adae318684265
nargysc <eliasnargiz@gmail.com>
2024-03-06 03:57:28 +0200

    Fixed click of blood

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ window.addEventListener('mousemove',function(e){
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
 })
+
+window.addEventListener('mousemove',function(e){
+    blood.style.left = e.pageX + "px";
+    blood.style.top = e.pageY + "px";
+})
+
+blood.style.display = 'none';
+window.addEventListener('click', function(e){
+        blood.style.display = `block`;
+   setTimeout(function(){
+    blood.style.display = `none`;
+   },1000)
+   audio.play()
+})
